bdb27beed4678a4192d54e88b835696a97f58f50
Pacheca <mpacheca7@gmail.com>
2025-04-23 19:48:47 +0200

    findBooksByUsn

--- src/main/java/IronLibrary/demo/repositories/BookRepositoryMiguel.java
+++ src/main/java/IronLibrary/demo/repositories/BookRepositoryMiguel.java
@@ -3,5 +3,9 @@ package IronLibrary.demo.repositories;
 import IronLibrary.demo.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepositoryMiguel extends JpaRepository<Book, Integer> {
+import java.util.List;
+
+public interface BookRepositoryMiguel extends JpaRepository<Book, String> {
+    // List books by usn:
+    List<Book> findBooksByUsn (String usn);
 }
