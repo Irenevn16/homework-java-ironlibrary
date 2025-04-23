9a5f275d34bff3f2e58f3a0b5b6339952885278e
Irenevn16 <irenevn16@gmail.com>
2025-04-23 18:11:02 +0200

    primeros métodos

--- src/main/java/IronLibrary/demo/models/Student.java
+++ src/main/java/IronLibrary/demo/models/Student.java
@@ -2,5 +2,44 @@ package IronLibrary.demo.models;
 import jakarta.persistence.*;
 
 @Entity
-public class Student {
+@Table (name= "student")
+public class Student { //lo de inherot?
+
+    @Id
+    private String usn; //Universal Student Number-unique identifier
+
+    private String name;
+
+    public Student(){
+
+    }
+
+    public Student(String usn, String name) {
+        this.usn = usn;
+        this.name = name;
+    }
+
+    public String getUsn() {
+        return usn;
+    }
+
+    public void setUsn(String usn) {
+        this.usn = usn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "usn='" + usn + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
