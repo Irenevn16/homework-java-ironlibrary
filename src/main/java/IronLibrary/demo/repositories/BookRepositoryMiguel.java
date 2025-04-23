fe3b843a3493940154317bebfa5d7b355b8f4317
Pacheca <mpacheca7@gmail.com>
2025-04-23 20:06:35 +0200

    BookRepositoryMiguel

--- src/main/java/IronLibrary/demo/repositories/BookRepositoryMiguel.java
+++ src/main/java/IronLibrary/demo/repositories/BookRepositoryMiguel.java
@@ -2,10 +2,16 @@ package IronLibrary.demo.repositories;
 
 import IronLibrary.demo.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BookRepositoryMiguel extends JpaRepository<Book, String> {
     // List books by usn:
-    List<Book> findBooksByUsn (String usn);
+    @Query(value = "SELECT Book.tittle FROM Book " +
+            "INNER JOIN Issue ON Book.isbn = Issue.issue_book " +
+            "WHERE Issue.issue_student = :usn",
+            nativeQuery = true)
+    List<String> findBookTitlesByStudentUsn(@Param("usn") String usn);
 }
