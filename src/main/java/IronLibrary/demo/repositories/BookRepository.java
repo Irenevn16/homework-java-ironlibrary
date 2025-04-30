ff85c6ad97404509c8f7c9df471ea8aaef3b8598
Irenevn16 <irenevn16@gmail.com>
2025-04-30 16:59:03 +0200

    uml subido

--- src/main/java/IronLibrary/demo/repositories/BookRepository.java
+++ src/main/java/IronLibrary/demo/repositories/BookRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
